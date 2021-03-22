2b51cc337d1a27737a5602ef198ec4c8f91ee116
fanwise <fanwise.901@gmail.com>
2021-03-22 22:06:55 +0800

    init project

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Wei fan on 2021/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Budget
+//
+//  Created by Wei fan on 2021/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
